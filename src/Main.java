1e0585e1e2dd3ce7a46ab7de5620f4f586eadc43
northinrtm <artsdurden@gmail.com>
2022-08-04 14:50:27 +0300

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("1 задача:");
+        {int clientOS = 1;
+        if(clientOS == 1);{
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }}
+        System.out.println("\n" + "2 задача:");
+
+        }
     }
-}
