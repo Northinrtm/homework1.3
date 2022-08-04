29ed24b56f53dc1f6ecd4d59506c43b9ee3b778f
northinrtm <artsdurden@gmail.com>
2022-08-04 15:44:58 +0300

    5 задача

--- src/Main.java
+++ src/Main.java
@@ -36,5 +36,49 @@ public class Main {
             if ((20 < deliveryDistance) && (deliveryDistance <= 60)) System.out.println("Потребуется дней: " + 2);
             if ((60 < deliveryDistance) && (deliveryDistance <= 100)) System.out.println("Потребуется дней: " + 3);
             }
+        System.out.println("\n" + "5 задача:");
+        {
+            int monthNumber = 13;
+            switch (monthNumber) {
+                case 1:
+                    System.out.println("Январь");
+                    break;
+                case 2:
+                    System.out.println("Фефраль");
+                    break;
+                case 3:
+                    System.out.println("Март");
+                    break;
+                case 4:
+                    System.out.println("Апрель");
+                    break;
+                case 5:
+                    System.out.println("Май");
+                    break;
+                case 6:
+                    System.out.println("Июнь");
+                    break;
+                case 7:
+                    System.out.println("Июль");
+                    break;
+                case 8:
+                    System.out.println("Август");
+                    break;
+                case 9:
+                    System.out.println("Сентябрь");
+                    break;
+                case 10:
+                    System.out.println("Октябрь");
+                    break;
+                case 11:
+                    System.out.println("Ноябрь");
+                    break;
+                case 12:
+                    System.out.println("Декабрь");
+                    break;
+                default:
+                    System.out.println("Такого месяца не существует.");
+            }
+        }
         }
 }
