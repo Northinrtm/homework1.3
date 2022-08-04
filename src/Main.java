a1feb2a9c18beadf599fae8b9c9185718d4bce2d
northinrtm <artsdurden@gmail.com>
2022-08-04 15:52:44 +0300

    5 задача

--- src/Main.java
+++ src/Main.java
@@ -41,40 +41,24 @@ public class Main {
             int monthNumber = 13;
             switch (monthNumber) {
                 case 1:
-                    System.out.println("Январь");
-                    break;
                 case 2:
-                    System.out.println("Фефраль");
+                case 12:
+                    System.out.println("Зима");
                     break;
                 case 3:
-                    System.out.println("Март");
-                    break;
                 case 4:
-                    System.out.println("Апрель");
-                    break;
                 case 5:
-                    System.out.println("Май");
+                    System.out.println("Весна");
                     break;
                 case 6:
-                    System.out.println("Июнь");
-                    break;
                 case 7:
-                    System.out.println("Июль");
-                    break;
                 case 8:
-                    System.out.println("Август");
+                    System.out.println("Лето");
                     break;
                 case 9:
-                    System.out.println("Сентябрь");
-                    break;
                 case 10:
-                    System.out.println("Октябрь");
-                    break;
                 case 11:
-                    System.out.println("Ноябрь");
-                    break;
-                case 12:
-                    System.out.println("Декабрь");
+                    System.out.println("Осень");
                     break;
                 default:
                     System.out.println("Такого месяца не существует.");
