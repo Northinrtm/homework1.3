14abc5b4af70c4e45fe5d930dfef51e19f565383
northinrtm <artsdurden@gmail.com>
2022-08-04 14:59:11 +0300

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("1 задача:");
-        {int clientOS = 1;
-        if(clientOS == 1);{
-            System.out.println("Установите версию приложения для Android по ссылке");
-        }}
+        {
+            int clientOS = 1;
+            if (clientOS == 1) ;
+            {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
         System.out.println("\n" + "2 задача:");
 
+        {
+            int clientOS = 1;
+            int clientDeviceYear = 2014;
+            if (clientOS == 1) {
+                if (clientDeviceYear < 2015)
+
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+
+                else System.out.println("Установите версию приложения для Android по ссылке");}
         }
-    }
+            }
+        }
