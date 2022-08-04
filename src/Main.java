0f086f57ca6d9c879047d956701ebc345a23813e
northinrtm <artsdurden@gmail.com>
2022-08-04 15:18:42 +0300

    3 задача

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,15 @@ public class Main {
 
                 else System.out.println("Установите версию приложения для Android по ссылке");}
         }
+        System.out.println("\n" + "3 задача:");
+        {
+            int year = 2022;
+            if (year % 400 == 0)
+            System.out.println(year + " год является високосным.");
+            else if ((year % 4 == 0) && (year % 100 != 0)) {
+                System.out.println(year + " год является високосным.");
+            }else System.out.println(year + " год не является високосным.");
+        }
+
             }
         }
